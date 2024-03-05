a8d452e75dfe359818bbfa9c049c27e1ea2606c4
bhushankhope <bhushankhope@gmail.com>
2024-03-05 14:38:41 -0500

    add animations to menu

--- script.js
+++ script.js
@@ -1,35 +1,23 @@
-const mobileNavMenu = document.querySelector('.mobile-nav-menu');
-const navMenuBtnIcon = document.querySelector('.mobile-nav-menu-btn > img');
 const navMenuBtn = document.querySelector('.mobile-nav-menu-btn');
+const navMenu = document.querySelector('.mobile-nav-menu');
 
-// Logic to handle the expand/collapse of the menu
+// Logic to handle the open/close of the mobile menu window
+navMenuBtn.addEventListener('click', () => {
+  navMenuBtn.classList.toggle('menu-opened');
+  navMenu.classList.add('animated');
+});
+
+// Logic to handle the expand/collapse of the menu items
 var toggles = document.querySelectorAll('.toggle');
 toggles.forEach(function (toggle) {
   toggle.addEventListener('click', function () {
-    // Adds margin to the menu item heading
-    toggle.classList.toggle('mb-3');
-    // Rotates the arrow
-    toggle.children[0].classList.toggle('rotate-180');
-    var submenu = this.nextElementSibling;
-    submenu.classList.toggle('grid');
+    this.classList.toggle('item-open');
+    this.nextElementSibling.classList.add('animated')
   });
 });
 
-// Logic to handle the open/close of the mobile menu
-navMenuBtn.addEventListener('click', () => {
-  navMenuBtn.classList.toggle('open');
-  mobileNavMenu.classList.toggle('unhide');
-  navMenuBtn.classList.toggle('menu-open-btn');
-  if (navMenuBtnIcon.src.includes('hamburger')) {
-    navMenuBtnIcon.src = 'assets/images/close.svg';
-  } else {
-    navMenuBtnIcon.src = 'assets/images/hamburger.svg';
-  }
-});
-
 function hideMenu() {
-  mobileNavMenu.classList.remove('unhide');
-  navMenuBtnIcon.src = 'assets/images/hamburger.svg';
+  navMenuBtn.classList.remove('menu-opened');
 }
 
 function largeScreenMenuButtonClickHandler(event) {
