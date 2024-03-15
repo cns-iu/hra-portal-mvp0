83160c630ca57b10564bedd4cbbf95bf071dacb5
bhushankhope <bhushankhope@gmail.com>
2024-03-15 11:06:58 -0400

    add scroll logic

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const navMenuBtn = document.querySelector('.mobile-nav-menu-btn');
 const navMenu = document.querySelector('.mobile-nav-menu');
+const banner = document.getElementById('collab-banner');
 
 // Logic to handle the open/close of the mobile menu window
 navMenuBtn.addEventListener('click', () => {
@@ -32,3 +33,23 @@ function largeScreenMenuButtonClickHandler(event) {
     event.target.classList.add(openClass);
   }
 }
+
+// Logic to handle the scrolling of collaborators banner - (WIP)
+let scrollingHorizontally = true;
+
+banner.addEventListener("wheel", (event) => {
+  event.deltaX = 0
+  if (scrollingHorizontally) {
+    banner.scrollBy({
+      left: event.deltaY < 0 ? -70 : 70,
+    });
+    event.preventDefault();
+    if (banner.scrollLeft >= banner.scrollWidth - banner.clientWidth - 100 && event.deltaY > 0) {
+      scrollingHorizontally = false;
+    } else if (banner.scrollLeft === 0 && event.deltaY < 0) {
+      scrollingHorizontally = false;
+    }
+  } else {
+    scrollingHorizontally = true;
+  }
+});
