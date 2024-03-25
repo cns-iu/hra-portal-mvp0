441132ddd06921a3f28bc4ef95129a8785cce7e5
bhushankhope <bhushankhope@gmail.com>
2024-03-25 13:49:26 -0400

    style largest web menu

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const navMenuBtn = document.querySelector('.mobile-nav-menu-btn');
 const navMenu = document.querySelector('.mobile-nav-menu');
 const banner = document.getElementById('collab-banner');
+const webMenuBar = document.querySelector('.web-nav-menu');
+const webMenus = document.querySelectorAll('.menu-wrapper > div');
+const openClass = 'open';
 
 // Logic to handle the open/close of the mobile menu window
 navMenuBtn.addEventListener('click', () => {
@@ -21,19 +24,34 @@ function hideMenu() {
   navMenuBtn.classList.remove('menu-opened');
 }
 
-function largeScreenMenuButtonClickHandler(event, i) {
-  const openClass = 'open';
+// Logic to close the menu when clicked on the toolbar icon
+const img = document.querySelector('.top-navigation > img');
+const body = document.getElementsByTagName('body');
+img.addEventListener('click', removeOpenClass);
+
+document.addEventListener('click', (e) => {
+  webMenus.forEach((menu) => {
+    if (!menu.contains(e.target) && !webMenuBar.contains(e.target)) {
+      removeOpenClass();
+    }
+  })
+})
+
+// Function to remove open class from the buttons and menus
+function removeOpenClass() {
   const buttons = document.querySelectorAll('.web-nav-menu > .top-navigation > .menu-btns > button');
-  const menus = document.querySelectorAll('.menu-wrapper > div');
-  const isClosed = !event.target.classList.contains(openClass);
-  const elements = [...buttons, ...menus]
+  const elements = [...buttons, ...webMenus]
   for (const el of elements) {
     el.classList.remove(openClass);
   }
+}
 
+function largeScreenMenuButtonClickHandler(event, i) {
+  const isClosed = !event.target.classList.contains(openClass);
+  removeOpenClass();
   if (isClosed) {
     event.target.classList.add(openClass);
-    menus[i].classList.add(openClass);
+    webMenus[i].classList.add(openClass);
   }
 }
 
